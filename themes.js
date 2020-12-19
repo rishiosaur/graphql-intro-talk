a2fb37ea30c72e9e63b55367d2c494ae80965edb
Rishi Kothari <itsrishikothari@gmail.com>
2020-12-19 12:42:17 -0500

    Add themes

---
+++ themes.js
@@ -0,0 +1,129 @@
+
+export const theme = {
+    fonts: {
+      body: '"Inter", system-ui, sans-serif',
+      heading: '"Inter", sans-serif',
+      monospace: "Fira Code,Jetbrains Mono, monospace",
+    },
+    colors: {
+      text: "#FFF",
+      background: "#000",
+      primary: "#e3116c",
+    },
+    styles: {
+      CodeSurfer: {
+        tokens: {
+          "comment prolog doctype cdata": {
+            color: "#999988",
+            fontStyle: "italic",
+          },
+          namespace: {
+            opacity: 0.7,
+          },
+          "string attr-value": {
+            color: "#e3116c",
+          },
+          "punctuation operator": {
+            color: "#FFF",
+          },
+          "entity url symbol number boolean variable constant property regex inserted": {
+            color: "#36acaa",
+          },
+          "atrule keyword attr-name selector": {
+            color: "#00a4db",
+          },
+          "function deleted tag": {
+            color: "#d73a49",
+          },
+          "function-variable": {
+            color: "#6f42c1",
+          },
+          "tag selector": {
+            color: "#00009f",
+          },
+        },
+        title: {
+          backgroundColor: "#000",
+          color: "#FFF",
+        },
+        subtitle: {
+          color: "#000",
+          backgroundColor: "#fff",
+        },
+        pre: {
+          color: "#FFF",
+          backgroundColor: "#000",
+        },
+        code: {
+          color: "#FFF",
+          backgroundColor: "#000",
+        },
+      },
+    },
+  };
+  
+  export const pres = {
+    fonts: {
+      body: '"Inter", system-ui, sans-serif',
+      heading: '"Inter", sans-serif',
+      monospace: "'Inter'",
+    },
+    colors: {
+      text: "#FFF",
+      background: "#000",
+      primary: "#e3116c",
+    },
+    styles: {
+      CodeSurfer: {
+        tokens: {
+          "comment prolog doctype cdata": {
+            color: "#999988",
+            fontStyle: "italic",
+          },
+          namespace: {
+            opacity: 0.7,
+          },
+          "string attr-value": {
+            color: "#e3116c",
+          },
+          "punctuation operator": {
+            color: "#FFF",
+          },
+          "entity url symbol number boolean variable constant property regex inserted": {
+            color: "#36acaa",
+          },
+          "atrule keyword attr-name selector": {
+            color: "#00a4db",
+          },
+          "function deleted tag": {
+            color: "#d73a49",
+          },
+          "function-variable": {
+            color: "#6f42c1",
+          },
+          "tag selector": {
+            color: "#00009f",
+          },
+        },
+        title: {
+          backgroundColor: "#000",
+          color: "#FFF",
+          fontSize:"150%"
+        },
+        subtitle: {
+          color: "#000",
+          backgroundColor: "#fff",
+        },
+        pre: {
+          color: "#FFF",
+          backgroundColor: "#000",
+        },
+        code: {
+          color: "#FFF",
+          backgroundColor: "#000",
+          fontFamily:"system-ui",
+          lineHeight: 1.6,
+        },
+      },
+    },
+  };
